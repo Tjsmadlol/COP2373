5717b61286770790732b95f2a75d5fb09a8411aa
Tjsmadlol <tjsmadlol@aol.com>
2026-02-10 15:25:52 -0500

    Changed the doc_string_converter to reflect the new assignment.

--- COP2373/doc_string_converter.py
+++ COP2373/doc_string_converter.py
@@ -1,17 +1,17 @@
 import inspect
-import TimothyStrong_ProgrammingExercise_1  # replace with your assignment name (without .py)
+import TimothyStrong_Programming_Exercise_2  # replace with your assignment name (without .py)
 
 # replace docstring_example with your assignment name in the next 2 lines of code
-with open("TimothyStrong_ProgrammingExercise_1.txt", "w") as doc:
-    doc.write(f"# Technical Design Document: {TimothyStrong_ProgrammingExercise_1.__name__}\n\n")
+with open("TimothyStrong_Programming_Exercise_2.txt", "w") as doc:
+    doc.write(f"# Technical Design Document: {TimothyStrong_Programming_Exercise_2.__name__}\n\n")
     # replace with your name, the date, and the description of the program
     doc.write(f"# Name: Timothy Strong\n")
-    doc.write(f"# Date: January 15, 2026\n")
+    doc.write(f"# Date: February 10, 2026\n")
     doc.write(
-        f"# Program Description: This program simulates the pre-sale of a limited number of cinema tickets and displays the total buyers once all tickets are bought.\n\n")
+        f"# Program Description: This program This program asks the user to enter an email message It scans the message for 30 common spam words/phrases.Each time a spam word appears, 1 point is added to the spam score providing a overall spam detecting score.\n\n")
 
     # replace docstring_example with your assignment name
-    for name, func in inspect.getmembers(TimothyStrong_ProgrammingExercise_1, inspect.isfunction):
+    for name, func in inspect.getmembers(TimothyStrong_Programming_Exercise_2, inspect.isfunction):
         doc.write(f"## Function: {name}\n")
         doc.write(f"{inspect.getdoc(func)}\n\n")
 
